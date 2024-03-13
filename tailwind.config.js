220ddb3ded56fdb8beddec8ca5b6adace8274a84
WalanyCosta <walanybnegro@gmail.com>
2024-03-13 17:51:57 +0100

    style: add colors of the projects

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 module.exports = {
   content: ["index.html", "style.css", 'main.js'],
   theme: {
+    colors: {
+      'white': '#FFFFFF',
+      'gray': {
+        200: '#F6F7F8'
+      },
+      'black': '#000',
+      'red': '#FF3131',
+      'green': '#54CC0A' 
+    },
     extend: {},
   },
   plugins: [],
