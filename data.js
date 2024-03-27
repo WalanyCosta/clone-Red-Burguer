64b15c94705152ab6c7a4463df787d88940febd5
WalanyCosta <walanybnegro@gmail.com>
2024-03-27 17:28:11 +0100

    style: add animation in buttons

---
+++ data.js
@@ -0,0 +1,80 @@
+export function data() {
+  return [
+    {
+      id: 1,
+      title: "Cheese Burger Duplo",
+      paragraph:
+        "Pão levinho de fermentação natural da Trigou, burger 160g, queijo prato e maionese da casa",
+      price: 35.0,
+      url: "../images/hamb-1.png",
+    },
+    {
+      id: 2,
+      title: "Cheese Burger",
+      paragraph:
+        "Pão levinho de fermentação natural da Trigou, burger 160g, queijo prato e maionese da casa",
+      price: 35.0,
+      url: "../images/hamb-2.png",
+    },
+    {
+      id: 3,
+      title: "Smash Burger",
+      paragraph:
+        "Pão levinho de fermentação natural da Trigou, burger 160g, queijo prato e maionese da casa",
+      price: 35.0,
+      url: "../images/hamb-3.png",
+    },
+    {
+      id: 4,
+      title: "Cheese Bacon",
+      paragraph:
+        "Pão levinho de fermentação natural da Trigou, burger 160g, queijo prato e maionese da casa",
+      price: 35.0,
+      url: "../images/hamb-4.png",
+    },
+    {
+      id: 5,
+      title: "Cheese Burger Duplo",
+      paragraph:
+        "Pão levinho de fermentação natural da Trigou, burger 160g, queijo prato e maionese da casa",
+      price: 35.0,
+      url: "../images/hamb-5.png",
+    },
+    {
+      id: 6,
+      title: "Cheese Burger Duplo",
+      paragraph:
+        "Pão levinho de fermentação natural da Trigou, burger 160g, queijo prato e maionese da casa",
+      price: 35.0,
+      url: "../images/hamb-6.png",
+    },
+    {
+      id: 7,
+      title: "Cheese Burger Duplo",
+      paragraph:
+        "Pão levinho de fermentação natural da Trigou, burger 160g, queijo prato e maionese da casa",
+      price: 35.0,
+      url: "../images/hamb-7.png",
+    },
+    {
+      id: 8,
+      title: "Cheese Burger Duplo",
+      paragraph:
+        "Pão levinho de fermentação natural da Trigou, burger 160g, queijo prato e maionese da casa",
+      price: 35.0,
+      url: "../images/hamb-8.png",
+    },
+    {
+      id: 9,
+      title: "Cheese Burger Duplo",
+      price: 35.0,
+      url: "../images/refri-1.png",
+    },
+    {
+      id: 10,
+      title: "Cheese Burger Duplo",
+      price: 35.0,
+      url: "../images/refri-2.png",
+    },
+  ];
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html", "style.css", 'main.js'],
+  content: ["index.html", "style.css", "main.js"],
   theme: {
     colors: {
-      'white': '#FFFFFF',
-      'gray':'#F6F7F8',
-      'black': '#000',
-      'red': '#FF3131',
-      'green': '#54CC0A' 
+      white: "#FFFFFF",
+      gray: "#F6F7F8",
+      black: "#000",
+      red: "#FF3131",
+      green: "#54CC0A",
     },
     extend: {
       backgroundImage: {
-        'hero': "url('/images/bg.png')"
-      }
+        hero: "url('/images/bg.png')",
+      },
     },
   },
   plugins: [],
-}
+};
